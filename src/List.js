859666ecb537c7dde8ececee4577265de939db8a
Hatrison <vadimulanovskiy@gmail.com>
2023-04-08 01:22:30 +0300

    Refactored doubly linked list into array list

--- src/List.js
+++ src/List.js
@@ -1,220 +1,70 @@
-class Node {
-  constructor(value, next = null, prev = null) {
-    this.value = value;
-    this.next = next;
-    this.prev = prev;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
+class List {
+  constructor(array = []) {
+    this.array = [...array];
   }
 
   length() {
-    let counter = 0;
-    let currentNode = this.head;
-
-    while (currentNode) {
-      counter++;
-      currentNode = currentNode.next;
-    }
-
-    return counter;
+    return this.array.length;
   }
 
   append(element) {
-    const newNode = new Node(element);
-
-    if (this.tail) {
-      this.tail.next = newNode;
-    }
-
-    newNode.prev = this.tail;
-    this.tail = newNode;
-
-    if (!this.head) {
-      this.head = newNode;
-    }
+    this.array.push(element);
   }
 
   insert(element, index) {
-    const length = this.length();
-
-    if (index < 0 || index > length) {
+    if (index < 0 || index > this.array.length) {
       console.log("Invalid index");
       return;
     }
 
-    if (index === length) {
-      this.append(element);
-      return;
-    }
-
-    const prev = this.getNode(index - 1);
-    const next = prev.next;
-    const newNode = new Node(element, next, prev);
-    prev.next = newNode;
-    next.prev = newNode;
+    this.array.splice(index, 0, element);
   }
 
   delete(index) {
-    const length = this.length();
-
-    if (index < 0 || index >= length) {
+    if (index < 0 || index > this.array.length - 1) {
       console.log("Invalid index");
       return;
     }
 
-    let value;
-
-    if (index === 0) {
-      value = this.head.value;
-      const next = this.head.next;
-      next.prev = null;
-      this.head = next;
-    } else if (index === length - 1) {
-      value = this.tail.value;
-      const prev = this.tail.prev;
-      prev.next = null;
-      this.tail = prev;
-    } else {
-      const node = this.getNode(index);
-      value = node.value;
-      const next = node.next;
-      const prev = node.prev;
-      prev.next = next;
-      next.prev = prev;
-    }
-
-    return value;
+    return this.array.splice(index, 1)[0];
   }
 
   deleteAll(element) {
-    let index = this.findFirst(element);
-
-    while (index !== -1) {
-      this.delete(index);
-      index = this.findFirst(element);
-    }
+    this.array = this.array.filter((item) => item !== element);
   }
 
-  getNode(index) {
-    if (index < 0 || index >= this.length()) {
+  get(index) {
+    if (index < 0 || index > this.array.length - 1) {
       console.log("Invalid index");
       return;
     }
 
-    let counter = 0;
-    let current = this.head;
-
-    while (counter < index) {
-      counter++;
-      current = current.next;
-    }
-
-    return current;
-  }
-
-  get(index) {
-    return this.getNode(index)?.value;
+    return this.array[index];
   }
 
   clone() {
-    const list = new LinkedList();
-
-    if (this.head) {
-      let counter = 0;
-      let current = this.head;
-
-      const length = this.length();
-      while (counter < length) {
-        list.append(current.value);
-        current = current.next;
-        counter++;
-      }
-    }
-
-    return list;
+    return new List(this.array);
   }
 
   reverse() {
-    let current = this.head;
-    let prev = null;
-    let next = null;
-
-    while (current) {
-      next = current.next;
-      prev = current.prev;
-
-      current.next = prev;
-      current.prev = next;
-
-      prev = current;
-      current = next;
-    }
-
-    this.tail = this.head;
-    this.head = prev;
+    this.array.reverse();
   }
 
   findFirst(element) {
-    if (!this.head) return -1;
-
-    let index = 0;
-    let current = this.head;
-
-    while (current?.value !== element) {
-      if (index === this.length()) {
-        return -1;
-      }
-
-      index++;
-      current = current.next;
-    }
-
-    return index;
+    return this.array.indexOf(element);
   }
 
   findLast(element) {
-    if (!this.head) return -1;
-
-    let index = this.length() - 1;
-    let current = this.tail;
-
-    while (current?.value !== element) {
-      if (index === -1) {
-        break;
-      }
-
-      index--;
-      current = current.prev;
-    }
-
-    return index;
+    return this.array.lastIndexOf(element);
   }
 
   clear() {
-    this.head = null;
-    this.tail = null;
+    this.array.length = 0;
   }
 
   extend(elements) {
-    if (!elements.head) return;
-
-    const cloneOfElements = elements.clone();
-
-    if (!this.head) {
-      this.head = cloneOfElements.head;
-      this.tail = cloneOfElements.tail;
-      return;
-    }
-
-    cloneOfElements.head.prev = this.tail;
-    this.tail.next = cloneOfElements.head;
-    this.tail = cloneOfElements.tail;
+    this.array = [...this.array, ...elements];
   }
 }
 
-module.exports = LinkedList;
+module.exports = List;
--- src/List.test.js
+++ src/List.test.js
@@ -1,200 +1,27 @@
-const LinkedList = require("./List");
+const List = require("./List");
 
 describe("Testing doubly linked list", () => {
-  test("Testing append method", () => {
-    const list = new LinkedList();
+  test("Testing append method", () => {});
 
-    list.append("a");
-    list.append("b");
-    list.append("c");
+  test("Testing length method", () => {});
 
-    expect(list.head).not.toBeNull();
-    expect(list.head.prev).toBeNull();
-    expect(list.head.next.value).toBe("b");
+  test("Testing insert method", () => {});
 
-    expect(list.getNode(1).prev.value).toBe("a");
-    expect(list.getNode(1).next.value).toBe("c");
+  test("Testing delete method", () => {});
 
-    expect(list.tail).not.toBeNull();
-    expect(list.tail.next).toBeNull();
-    expect(list.tail.prev.value).toBe("b");
-  });
+  test("Testing deleteAll method", () => {});
 
-  test("Testing length method", () => {
-    const list = new LinkedList();
+  test("Testing get method", () => {});
 
-    expect(list.length()).toBe(0);
+  test("Testing clone method", () => {});
 
-    list.append("a");
-    list.append("b");
-    list.append("c");
+  test("Testing reverse method", () => {});
 
-    expect(list.length()).toBe(3);
-  });
+  test("Testing findFirst method", () => {});
 
-  test("Testing insert method", () => {
-    const list = new LinkedList();
+  test("Testing findLast method", () => {});
 
-    list.append("a");
-    list.append("c");
+  test("Testing clear method", () => {});
 
-    list.insert("b", 1);
-
-    expect(list.get(-1)).toBeUndefined();
-    expect(list.get(5)).toBeUndefined();
-
-    expect(list.get(1)).toBe("b");
-    expect(list.getNode(1).prev.value).toBe("a");
-    expect(list.getNode(1).next.value).toBe("c");
-    expect(list.getNode(0).next.value).toBe("b");
-    expect(list.getNode(2).prev.value).toBe("b");
-  });
-
-  test("Testing delete method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-
-    const deletedCharacter = list.delete(1);
-
-    expect(deletedCharacter).toBe("b");
-
-    expect(list.delete(-1)).toBeUndefined();
-    expect(list.delete(5)).toBeUndefined();
-
-    expect(list.getNode(0).next.value).toBe("c");
-    expect(list.getNode(0).prev).toBeNull();
-    expect(list.getNode(1).prev.value).toBe("a");
-    expect(list.getNode(1).next).toBeNull();
-  });
-
-  test("Testing deleteAll method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-    list.append("a");
-
-    list.deleteAll("a");
-
-    expect(list.length()).toBe(2);
-
-    expect(list.getNode(0).value).toBe("b");
-    expect(list.getNode(1).value).toBe("c");
-    expect(list.findFirst("a")).toBe(-1);
-
-    list.deleteAll("f");
-    expect(list.getNode(0).value).toBe("b");
-    expect(list.getNode(1).value).toBe("c");
-  });
-
-  test("Testing get method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-
-    expect(list.get(-1)).toBeUndefined();
-    expect(list.get(5)).toBeUndefined();
-
-    expect(list.get(0)).toBe("a");
-    expect(list.get(1)).toBe("b");
-    expect(list.get(2)).toBe("c");
-  });
-
-  test("Testing clone method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-
-    const clonedList = list.clone();
-
-    expect(list.head).not.toBe(clonedList.head);
-    expect(list.head.next).not.toBe(clonedList.head.next);
-    expect(list.head.value).toBe(clonedList.head.value);
-    expect(list.get(1)).toBe(clonedList.get(1));
-  });
-
-  test("Testing reverse method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-
-    list.reverse();
-
-    expect(list.get(0)).toBe("c");
-    expect(list.get(1)).toBe("b");
-    expect(list.get(2)).toBe("a");
-
-    expect(list.head.prev).toBeNull();
-    expect(list.tail.next).toBeNull();
-  });
-
-  test("Testing findFirst method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-    list.append("a");
-
-    expect(list.findFirst("a")).toBe(0);
-    expect(list.findFirst("f")).toBe(-1);
-  });
-
-  test("Testing findLast method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-    list.append("a");
-
-    expect(list.findLast("a")).toBe(3);
-    expect(list.findLast("f")).toBe(-1);
-  });
-
-  test("Testing clear method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-
-    list.clear();
-
-    expect(list.length()).toBe(0);
-
-    expect(list.head).toBeNull();
-    expect(list.tail).toBeNull();
-  });
-
-  test("Testing extend method", () => {
-    const list = new LinkedList();
-
-    list.append("a");
-    list.append("b");
-    list.append("c");
-
-    const list2 = new LinkedList();
-
-    list2.append("d");
-    list2.append("f");
-
-    list.extend(list2);
-
-    expect(list.length()).toBe(5);
-    expect(list2.length()).toBe(2);
-
-    expect(list.getNode(2).next.value).toBe("d");
-    expect(list.getNode(3).prev.value).toBe("b");
-  });
+  test("Testing extend method", () => {});
 });
